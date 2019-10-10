feaf9088ee6ba1cb488e0616b3751ee2e59d03f7
milanjansen <m.jansen@sqits.nl>
2019-10-10 15:48:14 +0200

    message

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['as' => 'postcode-nl::', 'namespace' => 'Sqits\Postcode\Controllers'], function () {
-    Route::get('postcode/{postcode}/{houseNumber}/{houseNumberAddition?}', 'AddressController@show');
+Route::group(['as' => 'postcode-nl::'], function () {
+    Route::get('postcode/{postcode}/{houseNumber}/{houseNumberAddition?}', 'Sqits\Postcode\Controllers\AddressController@show');
 });
