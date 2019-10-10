8bb6055c1d7cb92d26f9516eff15ce81473dc2af
milanjansen <m.jansen@sqits.nl>
2019-10-10 15:29:49 +0200

    correct namespace

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['as' => 'postcode-nl::', 'namespace' => 'Controllers'], function () {
+Route::group(['as' => 'postcode-nl::', 'namespace' => 'Sqits\Postcode\Src\Controllers'], function () {
     Route::get('postcode/{postcode}/{houseNumber}/{houseNumberAddition?}', 'AddressController@show');
 });
